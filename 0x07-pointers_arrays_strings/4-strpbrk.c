06e95bb509863730834300e73779afdc40206c8d
gospearl <gospelagu8@gmail.com>
2023-03-07 07:29:07 -0800

    a  function that locates the first occurrence in a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurence of a character
+ * @s: string to be checked against
+ * @accept: string to checked
+ *
+ * Return: The remaining string after the first occurence
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				s += (j - 1);
+				return (s);
+			}
+			j++;
+		}
+	}
+}

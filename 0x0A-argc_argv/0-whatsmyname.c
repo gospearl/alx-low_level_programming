f589f83f65f7e0a0c3c653a20d7cf97267a16d21
gospearl <gospelagu8@gmail.com>
2023-03-11 03:25:06 -0800

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc - number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0(Success)
+ */
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
